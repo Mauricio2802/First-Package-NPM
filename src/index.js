ac871fa6775c6b56a7711dbd5be2fc60fb8ec49c
Mauricio2802 <vmcarrasco2810@gmail.com>
2021-09-24 20:42:29 -0500

    Cambio en frase

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const messages = [
   "No te esfuerces por ser buena persona, solo enfócate en ser mejor persona cada día.",
   "Frases sin contexto como esta son fáciles de malinterpretar, no te creas todo lo que te digan unas cuántas palabras, la vida es mucho más complejo que eso.",
   "La vida puede ser una mierda a veces, por eso la familia es importante.",
-  "Si sabes lo que tienes que hacer, no pares, pero recuerda que la vida no es sólo dinero y sexo.",
+  "Si sabes lo que tienes que hacer, no pares.",
   "Si el amor no es lo más importante en la vida, no sé que es lo más importante",
 ];
 
